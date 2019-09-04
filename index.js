28c3be149f8b567bf29ba6fb38287c8590735414
arnold_therigan <arnoldtherigan15@gmail.com>
2019-09-04 11:11:55 +0700

    update

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function ticTacToe(dimention) {
     let result = [];
-    let numbers;
     let arrX = 0
     let arrO = 0;
     for (let i = 0; i < dimention; i++) {
         result.push([]);
         for (let j = 0; j < dimention; j++) {
-            result[i].push(numbers = Math.floor(Math.random() * (9 - 0)) + 0);
+            let numbers = Math.floor(Math.random() * (9 - 0)) + 0;
+            result[i].push(numbers);
             if (result[i][j] % 2 === 0) {
                 result[i][j] = 'X';
                 arrX++;
