4b9b18405692fe8ad38b990c5804c969defbe5f7
evans <karlinevans333@gmail.com>
2019-09-04 23:59:32 +0700

    Tic Tac Toe revisi lebih singkat

--- index.js
+++ index.js
@@ -1,63 +1,51 @@
 
-const kamus = [
-    ["X", "O", "X", "O", "X", "O", "X", "O", "X"],
-    ["O", "X", "O", "X", "O", "X", "O", "X", "O"]
-]
-function ticTacToe (kamus){
+function ticTacToe (){
+    const kamus = [
+        ["X", "O", "X", "O", "X", "O", "X", "O", "X"],
+        ["O", "X", "O", "X", "O", "X", "O", "X", "O"]
+    ]
     let temp = kamus[Math.floor(Math.random() * 2)]
-    let arr = []
-    let random = 0
-    
-    for(let i = 0 ; i<3 ; i++){
-        arr.push([])
-        for(let j = 0 ; j<3 ; j++){
-            arr[arr.length-1].push(" ")
-        }
-    }
-
+    let arr = [
+        [' ',' ',' '],
+        [' ',' ',' '],
+        [' ',' ',' ']
+    ]
     let acakI = 0
     let acakJ = 0
     
-    while(true){
-        acakI = Math.floor(Math.random() * 3)
-        acakJ = Math.floor(Math.random() * 3)
-        if(arr[acakI][acakJ] == ' '){
-            arr[acakI][acakJ] = temp[0]
-            temp = temp.slice(1)
-        }
-        if(check(arr) != ''){
-            for(let i = 0 ; i<arr.length ; i++){
-                console.log(arr[i]);
+    for(var i = 0 ; i < 9 ; i++){
+        // Ngisi Random di tempat yg kosong
+        while(true){
+            acakI = Math.floor(Math.random() * 3)
+            acakJ = Math.floor(Math.random() * 3)
+            if(arr[acakI][acakJ] == ' '){
+                arr[acakI][acakJ] = temp[0]
+                temp = temp.slice(1)
+                break
             }
-            return check(arr)
         }
+
+        // Check Menang
+        for(let k = 0 ; k < 3 ; k++){
+            if(arr[k][0] == arr[k][1] && arr[k][1] == arr[k][2] && arr[k][0] != ' '){ // Check per Baris
+                return `${arr[0]} \n${arr[1]} \n${arr[2]} \n\n${arr[k][0]} Win`
+            }
+            else if(arr[0][k] == arr[1][k] && arr[1][k] == arr[2][k] && arr[0][k] != ' '){ // Check per Kolom
+                return `${arr[0]} \n${arr[1]} \n${arr[2]} \n\n${arr[0][k]} Win`
+            }
+        }
+        if(arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2] && arr[0][0] != ' '){ // Check diagonal kiri atas => kanan bawah
+            return `${arr[0]} \n${arr[1]} \n${arr[2]} \n\n${arr[0][0]} Win`
+        }
+        else if(arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0] && arr[0][2] != ' '){ // Check diagonal kanan atas => kiri bawah
+            return `${arr[0]} \n${arr[1]} \n${arr[2]} \n\n${arr[0][2]} Win`
+        }
+
+        // Check Draw
         if (temp.length == 0){
-            for(let i = 0 ; i<3 ; i++){
-                console.log(arr[i]);
-            }
-            return "Draw"
+            return `${arr[0]} \n${arr[1]} \n${arr[2]} \n\nDraw`
         }
     }
 }
 
-function check (arr){
-    for(let i = 0 ; i<3 ; i++){
-        if(arr[i][0] == arr[i][1] && arr[i][1] == arr[i][2] && arr[i][0] != ' '){
-            return `${arr[i][0]} Win`
-        }
-    }
-    for(let i = 0 ; i<3 ; i++){
-        if(arr[0][i] == arr[1][i] && arr[1][i] == arr[2][i] && arr[0][i] != ' '){
-            return `${arr[0][i]} Win`
-        }
-    }
-    if(arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2] && arr[0][0] != ' '){
-        return `${arr[0][0]} Win`
-    }
-    if(arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0] && arr[0][2] != ' '){
-        return `${arr[0][2]} Win`
-    }
-    return ''
-}
-
-console.log(ticTacToe(kamus));
+console.log(ticTacToe());
