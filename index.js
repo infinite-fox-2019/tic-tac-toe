ef1fad909ff212b32176da9e9f063e13a3d72606
straf16 <depoksugutamu621@gmail.com>
2019-09-04 19:06:12 +0700

    tictactoe release 1

--- index.js
+++ index.js
@@ -1,26 +1,69 @@
-function tictactoe() {
-    let arrays = [[], [], []];
-    let X0array = ['X', '0'];
-    let counterX = 0;
-    let counter0 = 0;
-    for (let i = 0; i < arrays.length; i++) {
-        for (let j = 0; j < 3; j++) {
-            if (counterX == 5) {
-                arrays[i].push('0');
-            } else if (counter0 == 5) {
-                arrays[i].push('X');
-            } else {
-                arrays[i].push(X0array[Math.round(Math.random() * 1)])
-            }
-            if (arrays[i][j] == 'X') {
-                counterX++;
-            } else {
-                counter0++;
-            }
+function checkWin(array) {
+    let check = [
+        `${array[0][0]}${array[0][1]}${array[0][2]}`,
+        `${array[1][0]}${array[1][1]}${array[1][2]}`,
+        `${array[2][0]}${array[2][1]}${array[2][2]}`,
+        `${array[0][0]}${array[1][0]}${array[2][0]}`,
+        `${array[0][1]}${array[1][1]}${array[2][1]}`,
+        `${array[0][2]}${array[1][2]}${array[2][2]}`,
+        `${array[0][0]}${array[0][1]}${array[0][2]}`,
+        `${array[0][0]}${array[1][1]}${array[2][2]}`,
+        `${array[0][2]}${array[1][1]}${array[2][0]}`,
+    ];
+    let winner = '';
+    for (let i = 0; i < check.length; i++) {
+        if (check[i] == 'XXX') {
+            return 'X is winner';
+        }
+        if (check[i] == '000') {
+            return '0 is winner';
         }
     }
+}
 
-    return arrays
+function tictactoe() {
+    let arrays = [
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+    ];
+    let X0array = ['X', '0'];
+    let pemenang = '';
+    let firstAttempt = X0array[Math.round(Math.random() * 1)];
+
+    for (let i = 0; i < 9; i++) {
+        let randomI = Math.floor(Math.random() * 3);
+        let randomJ = Math.floor(Math.random() * 3);
+        while (arrays[randomI][randomJ] != ' ' && i != 0) {
+            randomI = Math.floor(Math.random() * 3);
+            randomJ = Math.floor(Math.random() * 3);
+        }
+        if (i == 0) {
+            arrays[randomI][randomJ] = firstAttempt;
+        } else {
+            if (firstAttempt == 'X') {
+                if (i % 2 != 0) {
+                    arrays[randomI][randomJ] = X0array[1];
+                } else {
+                    arrays[randomI][randomJ] = X0array[0];
+                }
+            } else {
+                if (i % 2 != 0) {
+                    arrays[randomI][randomJ] = X0array[0];
+                } else {
+                    arrays[randomI][randomJ] = X0array[1];
+                }
+            }
+        }
+        pemenang = checkWin(arrays);
+        if (pemenang) {
+            console.log(arrays);
+            return pemenang;
+        } else if (!pemenang && i == 8) {
+            console.log(arrays);
+            return 'DRAW!!!'
+        }
+    }
 }
 
 console.log(tictactoe());
