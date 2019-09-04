37f58227c3f13044e36ddc7faa970b27b8a5eaa5
adityain98 <adityain98@gmail.com>
2019-09-04 10:27:53 +0700

    selesai

---
+++ index.js
@@ -0,0 +1,31 @@
+function ticTacToe() {
+    let array = [[],[],[]];
+    const data = 'XO';
+    let counterX = 0;
+    let counterO = 0;
+
+    for(let i = 0; i<array.length; i++){
+        for(let j = 0; j<3; j++){
+            let randomize = data.charAt(Math.floor(Math.random() * data.length));
+            if(counterO > 5){
+                array[i].push('X');
+            }
+            else if(counterX > 5){
+                array[i].push('O');
+            }
+            else{
+                array[i].push(randomize);
+                if(randomize == 'X'){
+                    counterX++
+                }
+                else{
+                    counterO++
+                }
+            }
+        }
+    }
+    return array;
+}
+
+console.log(ticTacToe());
+
