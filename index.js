5aff288f07ca3ae96379c83b778da223a3dc66b8
ashirogisen <m.rizki.aiman@gmail.com>
2019-09-04 20:09:01 +0700

    release 0 (fixed)

--- index.js
+++ index.js
@@ -11,33 +11,25 @@ function randomXO(length) {
 function tictactoe(row, col) {
     let arr_result = []
     let count_x = 0;
-    let count_o = 0
-    
+    let count_o = 0;
+
     for (let i = 0; i < row; i++) {
         arr_result.push([])
-
-        if (row*col % 2 !== 0) {
-            for (var j = 0; j < Math.ceil(col/2); j++) { //var digunakan agar bisa diakses pada loop k
+    
+        for (let j = 0; j < col; j++) {
+            if (count_x > Math.floor(row*col/2)) {
+                arr_result[i].push('O')
+            } else if (count_o > Math.floor(row*col/2)) {
+                arr_result[i].push('X')
+            } else {
                 arr_result[i].push(randomXO(1))
                 if (arr_result[i][j] === 'X') {
                     count_x++
-                    if (count_x > Math.floor(row*col/2)) {
-                        break;
-                    } 
-                } 
-            }
-
-            for (let k = 0; k < Math.floor(col/2); k++) {
-                arr_result[i].push(randomXO(1))
-                if (arr_result[i][j] === 'O') {
+                } else {
                     count_o++
-                    if (count_o > Math.floor(row*col/2)) {
-                        break;
-                    }
                 }
             }
         }
-
     }
 
     return arr_result
