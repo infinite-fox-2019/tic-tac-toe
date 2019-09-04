af6e2b4b2776f59e6ba71a7ada7670190fa97ff8
evans <karlinevans333@gmail.com>
2019-09-04 18:34:10 +0700

    Tic Tac Toe revisi

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const kamus = [
-    ["X", "X", "X", "X", "X", "O", "O", "O", "O"],
-    ["X", "X", "X", "X", "O", "O", "O", "O", "O"]
-]
 
+const kamus = [
+    ["X", "O", "X", "O", "X", "O", "X", "O", "X"],
+    ["O", "X", "O", "X", "O", "X", "O", "X", "O"]
+]
 function ticTacToe (kamus){
     let temp = kamus[Math.floor(Math.random() * 2)]
     let arr = []
@@ -11,12 +11,53 @@ function ticTacToe (kamus){
     for(let i = 0 ; i<3 ; i++){
         arr.push([])
         for(let j = 0 ; j<3 ; j++){
-            random = Math.floor(Math.random() * temp.length)
-            arr[arr.length-1].push(temp[random])
-            temp.splice(random,1)
+            arr[arr.length-1].push(" ")
         }
     }
-    return arr
+
+    let acakI = 0
+    let acakJ = 0
+    
+    while(true){
+        acakI = Math.floor(Math.random() * 3)
+        acakJ = Math.floor(Math.random() * 3)
+        if(arr[acakI][acakJ] == ' '){
+            arr[acakI][acakJ] = temp[0]
+            temp = temp.slice(1)
+        }
+        if(check(arr) != ''){
+            for(let i = 0 ; i<arr.length ; i++){
+                console.log(arr[i]);
+            }
+            return check(arr)
+        }
+        if (temp.length == 0){
+            for(let i = 0 ; i<3 ; i++){
+                console.log(arr[i]);
+            }
+            return "Draw"
+        }
+    }
+}
+
+function check (arr){
+    for(let i = 0 ; i<3 ; i++){
+        if(arr[i][0] == arr[i][1] && arr[i][1] == arr[i][2] && arr[i][0] != ' '){
+            return `${arr[i][0]} Win`
+        }
+    }
+    for(let i = 0 ; i<3 ; i++){
+        if(arr[0][i] == arr[1][i] && arr[1][i] == arr[2][i] && arr[0][i] != ' '){
+            return `${arr[0][i]} Win`
+        }
+    }
+    if(arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2] && arr[0][0] != ' '){
+        return `${arr[0][0]} Win`
+    }
+    if(arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0] && arr[0][2] != ' '){
+        return `${arr[0][2]} Win`
+    }
+    return ''
 }
 
 console.log(ticTacToe(kamus));
