92e79d5aba2327bcb6c5d92590733ac42f024eba
edwinsatya <edwinsatyayudistira@gmail.com>
2019-09-04 23:58:02 +0700

    coba2

--- index.js
+++ index.js
@@ -37,3 +37,68 @@ function ticTacToe (num) {
 };
 
 console.log(ticTacToe(3));
+
+function ticTacToeCobaVer2 (num) {
+    var board = [
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+    ];
+    var winCondition = [
+        [[0,0], [0,1], [0,2]],
+        [[1,0], [1,1], [1,2]],
+        [[2,0], [2,1], [2,2]],
+        [[0,0], [1,0], [2,0]],
+        [[0,1], [1,1], [2,1]],
+        [[0,2], [1,2], [2,2]],
+        [[0,0], [1,1], [2,2]],
+        [[0,2], [1,1], [2,0]]
+    ]
+    let belumAdaPemenang = true;
+    let XO = 'XO';
+    let random = XO[Math.floor(Math.random() * XO.length)];
+    let tempPosisiPlayer = {
+        'X' : [],
+        'O' : []
+    };
+
+    if (random === 'X') {
+        urutanPlayer = ['X', 'O'];
+    } else {
+        urutanPlayer = ['O', 'X'];
+    }
+    
+    while (belumAdaPemenang) {
+        for (let i = 0; i < urutanPlayer.length; i++) {
+            let posisiBelumAda = true;
+            loop1:
+            while (posisiBelumAda === true) {
+                var rowRandom = Math.floor(Math.random() * num);
+                var colRandom = Math.floor(Math.random() * num);
+                for (let keys in tempPosisiPlayer) {
+                    for (let j = 0; j < tempPosisiPlayer[keys].length; j++) {
+                        if (tempPosisiPlayer[keys][j][0] === rowRandom && tempPosisiPlayer[keys][j][1] === colRandom) {
+                            posisiBelumAda = false;
+                        }
+                    }
+                }
+                if (posisiBelumAda === true) {
+                    board[rowRandom][colRandom] = urutanPlayer[i];
+                    posisiBelumAda = false;
+                }                
+            }
+            tempPosisiPlayer[urutanPlayer[i]].push([rowRandom,colRandom]);
+        }
+        //belumAdaPemenang = false;
+        if (tempPosisiPlayer['X'].length >= 5 || tempPosisiPlayer['O'].length >= 5) {
+            belumAdaPemenang = false
+        }
+       
+    };
+    //console.log(winCondition[0][0])
+    //console.log(tempPosisiPlayer);
+    return board;
+};
+
+
+console.log(ticTacToeCobaVer2(3));
