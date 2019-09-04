c2755767ace62fa08abc73412d110a3a4fdbf6de
Kevin Anantha <kevin.anantha@gmail.com>
2019-09-04 20:24:28 +0700

    :anchor:

--- index.js
+++ index.js
@@ -1,36 +1,55 @@
 const generateGrid = row => {
-  const result = []
+  const array = [];
 
   for (let i = 0; i < row; i++) {
-    result.push([])
+    const temp = []
+    for (let j = 0; j < row; j++) {
+      temp.push('')
+    }
+    array.push(temp)
   }
 
-  return result
+  return array
+}
+
+const whosWin = board => {
+  // lines hori
+  if (board[0][0] == board[0][1] && board[0][0] == board[0][2] && board[0][1] == board[0][2] && board[0][2]) return true
+  if (board[1][0] == board[1][1] && board[1][0] == board[1][2] && board[1][1] == board[1][2] && board[1][2]) return true
+  if (board[2][0] == board[2][1] && board[2][0] == board[2][2] && board[2][1] == board[2][2] && board[2][2]) return true
+
+  // lines verti
+  if (board[0][0] == board[1][0] && board[0][0] == board[2][0] && board[1][0] == board[2][0] && board[2][0]) return true
+  if (board[0][1] == board[1][1] && board[0][1] == board[2][1] && board[1][1] == board[2][1] && board[2][1]) return true
+  if (board[0][2] == board[1][2] && board[0][2] == board[2][2] && board[1][2] == board[2][2] && board[2][2]) return true
+
+  // diagonal
+  if (board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[1][1] == board[2][2] && board[2][2]) return true
+  if (board[0][2] == board[1][1] && board[0][2] == board[2][0] && board[1][1] == board[2][0] && board[2][0]) return true
+  return false
 }
 
 const ticTacToe = row => {
   const grid = generateGrid(row)
-  const choices = 'XO'
-  let teamX = 0
-  let teamO = 0
+  const randomNum = Math.floor(Math.random() * 2)
+  const choices = randomNum == 1 ? 'XOXOXOXOX' : 'OXOXOXOXO'
+  console.log(choices[0] == 'X' ? 'X first' : 'O first')
   
-  for (let i = 0; i < row; i++) {
-    while (grid[i].length < row) {
-      const random = Math.floor(Math.random() * 2)
-      const pick = choices[random]
-      if (pick == 'X' && teamX < Math.ceil((row * row)) / 2) {
-        teamX++
-        grid[i].push(pick)
+  for (let i = 0; i < choices.length; i++) {
+    const randomI = Math.floor(Math.random() * row)
+    const randomJ = Math.floor(Math.random() * row)
+    if (!grid[randomI][randomJ]) {
+      grid[randomI][randomJ] = choices[i]
+      if (whosWin(grid) && i > 3) {
+        console.log(grid)
+        return `${choices[i]} Win!`
       }
-      if (pick == 'O' && teamO < Math.floor((row * row) / 2)) {
-        teamO++
-        grid[i].push(pick)
-      }
+    } else {
+      i -= 1
     }
   }
-  return grid
+  console.log(grid)
+  return `Draw!`
 }
 
 console.log(ticTacToe(3))
-console.log(ticTacToe(5))
-console.log(ticTacToe(7))
