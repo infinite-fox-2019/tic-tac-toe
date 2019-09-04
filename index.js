f638f4a6d8498d11a19356cf393d9ef31d9eb571
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-09-04 12:44:35 +0700

    done tictactoe

---
+++ index.js
@@ -0,0 +1,37 @@
+function tictactoe (matrix) {
+    let res = []
+    let countX = 0
+    let countO = 0
+    
+    for (let i=0; i<matrix; i++) {
+        res.push([])
+        let xo = 'xo'
+        for (let j=0; j<matrix; j++) {
+            if (countX < 5 && countO < 5) {
+                res[i].push(xo.charAt(Math.floor(Math.random() * xo.length)))
+                // console.log(countX, countO);
+                
+                if (res[i][j] === 'x') countX++
+                if (res[i][j] === 'o') countO++
+
+            } else {
+                
+                if (countX === 5) {
+                    res[i].push(xo[1])
+                    countO++
+                } else if (countO === 5) {
+                    res[i].push(xo[0]) 
+                    countX++
+                }
+            }
+            
+        }
+        
+    }
+    console.log(countX, countO);
+    
+return res
+}
+
+console.log(tictactoe(3));
+
