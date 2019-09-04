845a86a2dfeac91c1c16158a860ab065472d77b2
nadhiljanitra <nadhiljanitraa@gmail.com>
2019-09-04 20:17:21 +0700

    ticTacToeFinal_Nadhil

--- index.js
+++ index.js
@@ -1,39 +1,88 @@
-function ticTacToe(){
-let output = []
-  for (let i=0;i<3;i++){
-    let temp = []
-    for (let j=0;j<3;j++){
-      let value = Math.floor(Math.random()*2)
-      if (value == 1){
-        temp.push('X')
+function ticTacToe() {
+  let giliran = pemain(); // Pemain antara O atau X
+  let papan = board(); // Array board
+  let string = input(giliran); // Array input
+
+  for (let i = 0; i < string.length; i++) {
+    let randomIndexV = Math.floor(Math.random() * 3);
+    let randomIndexH = Math.floor(Math.random() * 3);
+    if (papan[randomIndexV][randomIndexH] === " ") {
+      papan[randomIndexV].fill(string[i], randomIndexH, randomIndexH + 1);
+      if (i > 3){
+        for (let i = 0; i< papan.length;i++){
+          if (papan[i][0] !== ' ' && papan[i][0] === papan[i][1] && papan[i][0] === papan[i][2]){
+            console.log(papan)
+            return `Pemenangnya adalah ${papan[i][0]}`
+          } else if (papan[0][i] !== ' ' && papan[0][i] === papan[1][i] && papan[0][i] === papan[2][i]){
+            console.log(papan)
+            return `Pemenangnya adalah ${papan[0][i]}`
+          } 
+        }
+        if (papan[1][1] !== ' ' && papan[1][1] === papan[0][0] && papan[1][1] == papan[2][2]){
+          console.log(papan)
+          return `Pemenangnya adalah ${papan[0][0]}`
+        }
+        if (papan[1][1] !== ' ' && papan[1][1] === papan[0][2] && papan[1][1] == papan[2][0]){
+          console.log(papan)
+          return `Pemenangnya adalah ${papan[1][1]}`
+        }
+      }
+    } else {
+      i--;
+    }
+  }
+  console.log(papan)
+  return "Permainan berakhir imbang";
+}
+
+function input(pemain) {
+  let output = [];
+  for (let i = 0; i < 9; i++) {
+    if (pemain == "X") {
+      if (i % 2 == 0) {
+        output[i] = "X";
       } else {
-        temp.push('O')
+        output[i] = "O";
+      }
+    } else {
+      if (i % 2 == 0) {
+        output[i] = "O";
+      } else {
+        output[i] = "X";
       }
     }
-    output.push(temp)
   }
+  return output;
+}
 
-  if (checkKondisi(output)) {
-    return output;
+function board() {
+  let temp = [];
+  let result = [];
+  let count = 0;
+  for (let i = 0; i < 9; i++) {
+    if (count < 3) {
+      temp.push(" ");
+      count++;
+    }
+    if (temp.length == 3) {
+      result.push(temp);
+      temp = [];
+      count = 0;
+    }
+  }
+  return result;
+}
+
+function pemain() {
+  let pemain = "";
+  let random = Math.floor(Math.random() * 2);
+  if (random == 1) {
+    pemain = "X";
   } else {
-    return ticTacToe();
+    pemain = "O";
   }
+  return pemain;
 }
 
-function checkKondisi(output){
-  let counter = 0
- for (let i=0;i<output.length;i++){
-   for (let j=0;j<output[i].length;j++){
-     if (output[i][j] === 'X'){
-       counter++
-     }
-   }
- }
- if (counter == 4 || counter == 5) {
-   return true;
- } else {
-   return false;
- }
-}
+console.log(ticTacToe());
 
-console.log(ticTacToe())
