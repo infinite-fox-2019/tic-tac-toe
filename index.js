1eba8a10eb7cfcf5cb7dac363f2d982c4cb319af
arnold_therigan <arnoldtherigan15@gmail.com>
2019-09-04 21:37:04 +0700

    update release 1

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
-function ticTacToe(dimention) {
-    let result = [];
-    let arrX = 0
-    let arrO = 0;
-    for (let i = 0; i < dimention; i++) {
-        result.push([]);
-        for (let j = 0; j < dimention; j++) {
-            let numbers = Math.floor(Math.random() * (9 - 0)) + 0;
-            result[i].push(numbers);
-            if (result[i][j] % 2 === 0) {
-                result[i][j] = 'X';
-                arrX++;
-                if (arrX > 4) {
-                    result[i][j] = 'O';
-                }
-            } else {
-                result[i][j] = 'O';
-                arrO++;
-                if (arrO > 4) {
-                    result[i][j] = 'X';
-                }
+function ticTacToe () {
+    let ticTacToeArr = 
+    [
+        [' ',' ',' '],
+        [' ',' ',' '],
+        [' ',' ',' ']
+    ];
+    const lib = 
+    [
+        ['X','O','X','O','X','O','X','O','X'],
+        ['O','X','O','X','O','X','O','X','O']
+    ];
+    let randomXO = lib[Math.floor(Math.random() * 2)];
+    while (true) {
+        let indeksI = Math.floor(Math.random() * 3);
+        let indeksJ = Math.floor(Math.random() * 3);
+        if (ticTacToeArr[indeksI][indeksJ] === ' ') {
+            ticTacToeArr[indeksI][indeksJ] = randomXO[0];
+            randomXO = randomXO.slice(1);
+        } else if (checkWinner(ticTacToeArr) != '') {
+            for (let i = 0; i < ticTacToeArr.length; i++) {
+                console.log(ticTacToeArr[i]);
             }
+            return checkWinner(ticTacToeArr);
+        } else if (randomXO.length === 0) {
+            for (let i = 0; i < ticTacToeArr.length; i++) {
+                console.log(ticTacToeArr[i]);
+            }
+            return 'DRAW!'
         }
     }
-    return result;
 }
-console.log(ticTacToe(3));
+function checkWinner (ticTacToeArr) {
+    for (let i = 0; i < 3; i++) {
+        if(ticTacToeArr[i][0] === ticTacToeArr[i][1] && ticTacToeArr[i][1] == ticTacToeArr[i][2] && ticTacToeArr[i][0] != ' ') {
+            return `${ticTacToeArr[i][0]} is The Winner!`            
+        }
+        if(ticTacToeArr[0][i] === ticTacToeArr[1][i] && ticTacToeArr[1][i] == ticTacToeArr[2][i] && ticTacToeArr[0][i] != ' ') {
+            return `${ticTacToeArr[0][i]} is The Winner!`            
+        }
+        if(ticTacToeArr[0][0] === ticTacToeArr[1][1] && ticTacToeArr[1][1] == ticTacToeArr[2][2] && ticTacToeArr[0][0] != ' ') {
+            return `${ticTacToeArr[0][0]} is The Winner!`            
+        }if(ticTacToeArr[0][2] === ticTacToeArr[1][1] && ticTacToeArr[1][1] == ticTacToeArr[2][0] && ticTacToeArr[0][2] != ' ') {
+            return `${ticTacToeArr[0][2]} is The Winner!`            
+        }
+    }
+    return '';
+}
+console.log(ticTacToe());
+
