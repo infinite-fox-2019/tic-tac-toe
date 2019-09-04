ebca33b91a673c75beade98f29502f7e5aaecf36
ericsudhartio <*ericsudhartio2509@gmail.com>
2019-09-04 16:29:10 +0700

    update bug

--- index.js
+++ index.js
@@ -3,22 +3,23 @@ function ticTacToe() {
     let XO = 'XO';
     let countX = 0;
     let countO = 0;
+    let middle = Math.round(3*3)/2
     for(let i=0; i<3; i++){
         let arr = [];
         for(let j=0; j<3; j++){
             let random = XO[Math.floor(Math.random() * XO.length)];
-            if(random==='X' && countX<Math.round((3*3)/2)){
+            if(random==='X' && countX<middle){
                 arr.push('X');
                 countX++
             }
-            if(random==='O' && countO<=Math.round((3*3)/2)) {
+            if(random==='O' && countO<=middle) {
                 arr.push('O');
                 countO ++
             }
         }
         resultArr.push(arr);
-    }
-    if(countO === 5 || countX === 5){
+    } 
+    if(resultArr[2].length === 3){
         return resultArr
     }else{
         return ticTacToe()
